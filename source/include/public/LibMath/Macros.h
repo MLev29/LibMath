cfb9c705510310e9332706f24d3639f532360fb1
MLev29 <ml.290620056@gmail.com>
2024-10-20 14:45:57 -0400

    Add macro file
    Add useful math & physic macros

---
+++ source/include/public/LibMath/Macros.h
@@ -0,0 +1,13 @@
+#pragma once
+
+// Angles
+#define PI 3.141592654f			// Float π
+#define TWO_PI 6.283185307f		// Float 2π
+
+#define DEG2RAD	0.01745329252f	// Float π / 180
+#define RAD2DEG 57.29577951f	// Float 180 / π
+
+#define E_CONSTANT 2.718281828f	// Float e
+
+// Physics
+#define GRAVITY_CONSTANT 9.81f	// Float earth gravity constant
